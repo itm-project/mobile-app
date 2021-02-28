c954efabeb2a25ec526f4549b8e077b9906f3ca1
Pitchakorn-W <pitchakorn.w@ku.th>
2021-02-28 23:11:50 +0700

    Test Login03

--- MITMIT/app/src/main/java/com/example/mitmit/Register.kt
+++ MITMIT/app/src/main/java/com/example/mitmit/Register.kt
@@ -3,10 +3,7 @@ package com.example.mitmit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +19,34 @@ class Register : AppCompatActivity() {
 
         val btnRegis : Button = findViewById(R.id.btnRegis)
         val cbRegis : CheckBox = findViewById(R.id.cbTerms)
+        val mail : EditText = findViewById(R.id.enteremail)
+        val user : EditText = findViewById(R.id.enterusername)
+        val pw : EditText = findViewById(R.id.enterpassword)
+        val cfpw : EditText = findViewById(R.id.confpassword)
+        val ad : EditText = findViewById(R.id.enteraddress)
 
-        btnRegis.setOnClickListener{
-            Toast.makeText(this,"Regis!",Toast.LENGTH_SHORT).show()
+        cbRegis.setOnCheckedChangeListener { buttonView, isChecked ->
+            
+            if(mail.text.trim().isEmpty() || user.text.trim().isEmpty() || pw.text.trim().isEmpty() || cfpw.text.trim().isEmpty() || ad.text.trim().isEmpty())
+            {
+                btnRegis.setOnClickListener{
+                    Toast.makeText(this,"Input Required",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else if(!isChecked && mail.text.trim().isNotEmpty() && user.text.trim().isNotEmpty() && pw.text.trim().isNotEmpty() && cfpw.text.trim().isNotEmpty() && ad.text.trim().isNotEmpty())
+            {
+                btnRegis.setOnClickListener{
+                    Toast.makeText(this,"Need to Accept Terms",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else if(isChecked && mail.text.trim().isNotEmpty() && user.text.trim().isNotEmpty() && pw.text.trim().isNotEmpty() && cfpw.text.trim().isNotEmpty() && ad.text.trim().isNotEmpty() )
+            {
+                btnRegis.setOnClickListener{
+                    Toast.makeText(this,"Regis!",Toast.LENGTH_SHORT).show()
+                }
+            }
+
         }
+
     }
 }
