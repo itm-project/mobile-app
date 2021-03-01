9211af3e6bdf0566ce9da52734150625b1cc0bb2
Pitchakorn-W <pitchakorn.w@ku.th>
2021-03-01 20:26:12 +0700

    Test Drawer01
    only Drawer -3-)/

---
+++ Drawer/app/src/main/java/com/example/drawer/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.drawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var homeFrag : HomeFragment
+    lateinit var proFrag : ProfileFragment
+    lateinit var hisFrag : HistoryFragment
+    lateinit var setFrag : SettingFragment
+    lateinit var logoutFrag : LogoutFragment
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val actionBar = supportActionBar
+        actionBar!!.title = "Navigation Drawer"
+
+        val drawerToggle : ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            (R.string.close),(R.string.open)
+        ) {
+
+        }
+
+        val nav_view : NavigationView = findViewById(R.id.nav_view)
+
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
+
+        homeFrag = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout,homeFrag)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        when (menuItem.itemId)
+        {
+            R.id.home -> {
+                homeFrag = HomeFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,homeFrag)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+
+            R.id.profile -> {
+                proFrag = ProfileFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,proFrag)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+
+            R.id.history -> {
+                hisFrag = HistoryFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,hisFrag)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+
+            R.id.setting -> {
+                setFrag = SettingFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,setFrag)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+
+            R.id.logout -> {
+                logoutFrag = LogoutFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,logoutFrag)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else
+        {
+            super.onBackPressed()
+        }
+    }
+}
